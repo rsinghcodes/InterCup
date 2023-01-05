fdbb4e94e0bb75b547275db1f73b3a7b355066c9
Raghvendra Singh <raghvendrrsingh@gmail.com>
2023-01-05 21:25:06 +0530

    Created separate frontend for Intercup app

--- server/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  MONGODB: process.env.MONGODB,
-  JWT_SECRET: process.env.JWT_SECRET,
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  BASE_URL: process.env.BASE_URL,
-};
